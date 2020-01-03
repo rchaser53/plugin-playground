7abb64d370acc3283591a54d2ef3e72d4f95db7c
rchaser53 <tayoshizawa29@gmail.com>
2020-01-03 13:44:01 +0900

    just add custom error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::process::Command;
 use std::str::from_utf8;
 
@@ -6,20 +7,48 @@ fn main() {
         Ok(ret) => {
             dbg!(from_utf8(&ret));
         }
-        Err(err) => panic!(err),
+        Err(err) => {
+            panic!("{}", err);
+        }
     };
 }
 
-fn execute() -> Result<Vec<u8>, String> {
+pub enum CustomError {
+    IoError(String),
+    Utf8Error(String),
+    ShellError(String),
+}
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CustomError::IoError(message)
+            | CustomError::Utf8Error(message)
+            | CustomError::ShellError(message) => write!(f, "{}", message),
+        }
+    }
+}
+
+impl From<std::io::Error> for CustomError {
+    fn from(e: std::io::Error) -> CustomError {
+        CustomError::IoError(e.to_string())
+    }
+}
+
+impl From<std::str::Utf8Error> for CustomError {
+    fn from(e: std::str::Utf8Error) -> CustomError {
+        CustomError::Utf8Error(e.to_string())
+    }
+}
+
+fn execute() -> Result<Vec<u8>, CustomError> {
     let stdout = std::process::Stdio::piped();
-    let result = Command::new("ls")
-        .stdout(stdout)
-        .args(&["-z"])
-        .output()
-        .unwrap();
+    let result = Command::new("ls").stdout(stdout).args(&["-z"]).output()?;
     if result.status.success() {
         Ok(result.stdout)
     } else {
-        Err(from_utf8(&result.stderr).unwrap().to_string())
+        Err(CustomError::ShellError(
+            from_utf8(&result.stderr)?.to_string(),
+        ))
     }
 }
